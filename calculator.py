3107aaad8274146a8d9a74badce6fa3167f98d04
weicheng <1159736841@qq.com>
2019-08-07 12:46:54 +0800

    完善资料

---
+++ calculator.py
@@ -0,0 +1,11 @@
+# 加法计算
+print('加法计算：1 + 1 = ',1 + 1)
+
+# 混合计算
+print('混合计算 (50 - 3*4)/4 = ',50- 3*4)
+
+# 变量赋值
+width = 30
+height = 40
+print('变量计算width*height=',width*height)
+
---
+++ comment.py
@@ -0,0 +1,40 @@
+# 第一个注释
+# 没有注释，你就是小猫，小狗
+print("注释：小明，笑一个") 
+print("Hello world!");
+print("Hello I am not 小明!\n");
+
+# 设flag是False
+# 设name是小明
+flag = False
+name = '小明'
+
+# 判断变量是否为 python 
+# 条件成立时设置标志为真  
+# 并输出欢迎信息
+# 条件不成立时输出变量名称
+print("注释：打个招呼") 
+if name == 'python':  
+    flag = True 
+    print('welcome boss\n')     
+else:
+    print(name+'你好！\n')     
+
+# 赋值整型变量
+# 浮点型
+# 字符串
+counter = 100 
+miles = 1000.0 
+name = "John I am ok\n" 
+
+# 输出整型变量
+# 浮点型
+# 字符串
+print("注释：我想看看你是什么，数字，小数还是字符还是小明？")  
+print(counter) 
+print(miles) 
+print(name[2:8]+'\n') 	
+
+# 我想计算
+print("注释：我想计算") 
+print(3*44+3**44) 	
